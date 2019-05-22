298283c0420ed90e6d1815f7f778693a2f7ba3f4
Antony O'Neill <antony@boclips.com>
2019-05-22 15:57:07 +0100

    Add webpack

---
+++ webpack-config/webpack.common.js
@@ -0,0 +1,54 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+const srcPath = path.resolve(__dirname, '../src');
+const distPath = path.resolve(__dirname, '../dist');
+
+module.exports = {
+  entry: {
+    'boclips-player-react': path.resolve(srcPath, 'BoclipsPlayer.tsx'),
+  },
+  output: {
+    filename: '[name].js',
+    path: distPath,
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.(ts|tsx)$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
+      },
+      {
+        test: /\.(less|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'less-loader',
+            options: {
+              javascriptEnabled: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      chunkFilename: '[id].css',
+      filename: '[name].css',
+    }),
+  ],
+};
