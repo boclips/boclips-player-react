c7bff5eb47bd4fbe219300bcfe046b75bc393d7d
Rebecca Appleyard <rebecca@boclips.com>
2019-05-23 13:40:52 +0100

    Added a demo for dummy BoclipsPlayer
    Signed-off-by: Antony O'Neill <antony@boclips.com>

---
+++ demo/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BoclipsPlayer } from '../src/BoclipsPlayer';
+
+const container = document.querySelector('#container');
+
+ReactDOM.render(<BoclipsPlayer/>, container);
---
+++ demo/webpack.config.js
@@ -0,0 +1,17 @@
+const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const common = require('../webpack-config/webpack.dev.js');
+
+const path = require('path');
+
+module.exports = merge(common, {
+  entry: {
+    'demo': path.resolve(__dirname, 'index.tsx')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+    }),
+  ],
+});
