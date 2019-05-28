9682bdee04bfe4c06a9ddf78b55de9ea2d23000c
Benjamin Gröhbiel <ben@boclips.com>
2019-05-28 17:21:09 +0100

    Updating boclips-player, separating out peer deps
    Signed-off-by: Antony O'Neill <antony@boclips.com>

--- src/test-support/setup.ts
+++ src/test-support/setup.ts
@@ -1,13 +1,9 @@
-import axios from 'axios';
-import AxiosLogger from 'axios-logger';
-import MockFetchVerify from './MockFetchVerify';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 beforeEach(() => {
-  (axios.interceptors.request as any).handlers = [];
-  (axios.interceptors.request as any).use(AxiosLogger.requestLogger);
-
-  MockFetchVerify.clear();
-
   // Can you believe JEST doesn't automatically clean up?
   document.body.innerHTML = '';
+
+  Enzyme.configure({ adapter: new Adapter() });
 });
