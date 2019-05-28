fe0449ea5ea994d3f76203fa5caef9a40856d64b
Antony O'Neill <antony@boclips.com>
2019-05-28 09:55:33 +0100

    Implement react wrapper & add demo
    [#166056503]

--- demo/index.tsx
+++ demo/index.tsx
@@ -1,7 +1,84 @@
+import { BoclipsPlayer } from 'boclips-player';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Player } from '../src/Player';
 
-const container = document.querySelector('#container');
+class ContainerApp extends React.Component<
+  any,
+  { heading: string; showPlayer: boolean }
+> {
+  public state = {
+    heading: 'Hello 123',
+    showPlayer: true,
+  };
 
-ReactDOM.render(<Player/>, container);
+  public render() {
+    return (
+      <>
+        <button onClick={this.changeHeading}>Change header</button>
+        <button onClick={this.togglePlayer}>Toggle Player</button>
+        {this.state.showPlayer && <SampleApp heading={this.state.heading} />}
+      </>
+    );
+  }
+
+  public changeHeading = () => {
+    this.setState({ heading: new Date().toISOString() });
+  };
+  public togglePlayer = () => {
+    this.setState(state => ({ showPlayer: !state.showPlayer }));
+  };
+}
+
+class SampleApp extends React.Component<{ heading: string }> {
+  private player: BoclipsPlayer;
+
+  public state = {
+    videoUri:
+      'https://teachers.staging-boclips.com/v1/videos/5c542ab85438cdbcb56ddceb',
+  };
+
+  public render() {
+    return (
+      <>
+        <h1>{this.props.heading}</h1>
+        <Player playerRef={this.getPlayerRef} videoUri={this.state.videoUri} />
+        <div>
+          <button onClick={this.loadVideoOne}>Load Video One</button>
+          <button onClick={this.loadVideoTwo}>Load Video Two</button>
+          <button onClick={this.loadVideoThree}>Load Video Three</button>
+        </div>
+        <div>
+          <button onClick={this.handlePlay}>Play</button>
+          <button onClick={this.handlePause}>Pause</button>
+        </div>
+      </>
+    );
+  }
+
+  private loadVideoOne = () =>
+    this.setState({
+      videoUri:
+        'https://teachers.staging-boclips.com/v1/videos/5c542ab85438cdbcb56ddceb',
+    });
+
+  private loadVideoTwo = () =>
+    this.setState({
+      videoUri:
+        'https://teachers.staging-boclips.com/v1/videos/5c542abf5438cdbcb56df0bf',
+    });
+
+  private loadVideoThree = () =>
+    this.setState({
+      videoUri:
+        'https://teachers.staging-boclips.com/v1/videos/5c7e6e3c93aafe1355ad8bf5',
+    });
+
+  private getPlayerRef = player => (this.player = player);
+
+  private handlePlay = () => this.player.play();
+
+  private handlePause = () => this.player.pause();
+}
+
+ReactDOM.render(<ContainerApp />, document.querySelector('#container'));
--- src/Player.tsx
+++ src/Player.tsx
@@ -1,20 +1,74 @@
-import { BoclipsPlayerFactory } from 'boclips-player';
+import { BoclipsPlayer, BoclipsPlayerFactory } from 'boclips-player';
 import * as React from 'react';
 
-export class Player extends React.Component {
+interface Props {
+  playerRef?: (player: BoclipsPlayer) => void;
+  videoUri?: string;
+  handlePlay?: () => Promise<void>;
+  handlePause?: () => void;
+}
+
+interface State {
+  error: boolean;
+}
+
+const noop: (args?: any) => any = () => {};
+
+export class Player extends React.Component<Props, State> {
   private container: HTMLDivElement;
   // @ts-ignore
   private player: BoclipsPlayer;
 
+  public state: State = {
+    error: false,
+  };
 
-  componentDidMount() {
+  public static defaultProps: Partial<Props> = {
+    videoUri: null,
+    playerRef: noop,
+    handlePlay: noop,
+    handlePause: noop,
+  };
+
+  public componentDidMount() {
     this.player = BoclipsPlayerFactory.get(this.container);
+    if (this.props.videoUri) {
+      // noinspection JSIgnoredPromiseFromCall
+      this.player.loadVideo(this.props.videoUri);
+    }
+    this.props.playerRef(this.player);
   }
 
-  render() {
+  public componentWillUnmount(): void {
+    this.player.destroy();
+  }
+
+  public componentDidUpdate(prevProps: Readonly<Props>) {
+    if (prevProps.videoUri !== this.props.videoUri) {
+      this.player.loadVideo(this.props.videoUri);
+    }
+  }
+
+  public componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
+    this.setState({ error: true });
+    console.error(
+      'An error occurred within the Player component.',
+      error,
+      errorInfo,
+    );
+  }
+
+  public render() {
+    if (this.state.error) {
+      return <div>An error has occurred.</div>;
+    }
+
     return (
-      <div ref={(container) => {this.container = container} }>Hello Second World</div>
-    )
+      <div
+        ref={container => {
+          this.container = container;
+        }}
+      />
+    );
   }
-
 }
