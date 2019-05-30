8945b9c66acbecefdb2243881d47597d01d4e1ff
Antony O'Neill <antony@boclips.com>
2019-05-30 15:48:36 +0100

    Update jest test-support

--- jest.config.js
+++ jest.config.js
@@ -126,7 +126,7 @@ module.exports = {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  // setupFiles: [],
+  setupFiles: ['./src/test-support/setup-env.ts'],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: ['./src/test-support/setup.ts'],
---
+++ src/test-support/setup-env.ts
@@ -0,0 +1,9 @@
+// @ts-ignore
+window.TextTrack = jest.fn();
+
+Object.defineProperty(HTMLElement.prototype, 'insertAdjacentElement', {
+    configurable: true,
+    value(_, element) {
+        this.appendChild(element);
+    },
+});
