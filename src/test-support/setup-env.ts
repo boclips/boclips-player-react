1d2261f3b5a2c2ce0cf683c9d10db1dd3617db48
Matt Jones <matt@boclips.com>
2020-04-06 09:08:34 +0100

    Use a ref hook for the player
    Also switch to eslint which will give us exhaustive dependency checks,
    which we were violating previously

--- src/test-support/setup-env.ts
+++ src/test-support/setup-env.ts
@@ -2,8 +2,8 @@
 window.TextTrack = jest.fn();
 
 Object.defineProperty(HTMLElement.prototype, 'insertAdjacentElement', {
-    configurable: true,
-    value(_, element) {
-        this.appendChild(element);
-    },
+  configurable: true,
+  value(_, element) {
+    this.appendChild(element);
+  },
 });
