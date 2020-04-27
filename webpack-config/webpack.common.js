3372d277f9a20c97b9c7b67ca2ac5028866e3646
Alex Glen <alex@boclips.com>
2020-04-27 17:35:02 +0100

    Bump boclips-player to 2.9
    [#172432778]

--- webpack-config/webpack.common.js
+++ webpack-config/webpack.common.js
@@ -1,5 +1,6 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 const srcPath = path.resolve(__dirname, '../src');
@@ -53,5 +54,11 @@ module.exports = {
       chunkFilename: '[id].css',
       filename: '[name].css',
     }),
+    new CopyPlugin([
+      {
+        from: './src/icons/youtube-sprite.svg',
+        to: 'static/assets',
+      }
+    ])
   ],
 };
