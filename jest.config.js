79aa01940edd17501a0e796facc3f07e7411de25
Marcin Janik <marcin@boclips.com>
2023-01-03 11:08:16 +0100

    Remove boclips-player from devDependencies
    The dev dependency version of bo player would override the regular
    dependency used in consumer's app (CourseSpark). It would mean consumers would never be
    able to use a version other than the one in devDeps, which kinda is what
    peerDeps are trying to enable.
    
    [#184072995]

--- jest.config.js
+++ jest.config.js
@@ -16,6 +16,7 @@ module.exports = {
     'tsx',
   ],
   moduleNameMapper: {
+    'boclips-player/dist/main.css': '<rootDir>/__mocks__/styleMock.ts',
     '\\.(css|less|scss)$': '<rootDir>/__mocks__/styleMock.ts',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.ts',
   },
