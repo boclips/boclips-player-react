b2b646bd70eb68bbb5edf8b1bff62095d9c401d0
Alex Glen <alex@boclips.com>
2023-07-05 20:55:24 +0100

    Add poster and use apiClient ref

--- src/NewPlayer.tsx
+++ src/NewPlayer.tsx
@@ -1,5 +1,10 @@
 import React, { ReactElement, useEffect, useRef, useState } from 'react';
-import { MediaCommunitySkin, MediaOutlet, MediaPlayer } from '@vidstack/react';
+import {
+  MediaCommunitySkin,
+  MediaOutlet,
+  MediaPlayer,
+  MediaPoster,
+} from '@vidstack/react';
 import 'vidstack/styles/defaults.css';
 import 'vidstack/styles/community-skin/video.css';
 import { ApiBoclipsClient } from 'boclips-api-client';
@@ -29,26 +34,29 @@ export const Player = ({ videoUrl, tokenFactory }: Props): ReactElement => {
   //   player?.current!.startLoading();
   // });
   const [src, setSrc] = useState<string>('');
-
+  const apiClient = useRef<ApiBoclipsClient>();
   console.log('hi');
   useEffect(() => {
     console.log('load-start');
     async function getMediaStream() {
-      const apiClient = await ApiBoclipsClient.create(axios, videoUrl.substring(0, videoUrl.indexOf('/v1')));
-      // original media event (`loadedmetadata`) is still available.
-      let id = videoUrl.substring(videoUrl.indexOf('/v1/videos/') + 11);
-      console.log(id);
-      const video = await apiClient.videos.get(
-        id,
+      apiClient.current = await ApiBoclipsClient.create(
+        axios,
+        videoUrl.substring(0, videoUrl.indexOf('/v1')),
       );
+      // original media event (`loadedmetadata`) is still available.
+      const id = videoUrl.substring(videoUrl.indexOf('/v1/videos/') + 11);
+      console.log(id);
+      const video = await apiClient.current.videos.get(id);
       const url = video.playback.links
         .hlsStream!.getOriginalLink()
         .replace('.mp4', '.m3u8');
       player?.current.setAttribute('src', url);
       player?.current.setAttribute('title', video.title);
-      // Video
-      const videoStream = url;
-      setSrc(videoStream);
+      player?.current.setAttribute(
+        'poster',
+        video.playback.links.thumbnail.getOriginalLink(),
+      );
+      setSrc(url);
     }
 
     getMediaStream();
@@ -57,6 +65,7 @@ export const Player = ({ videoUrl, tokenFactory }: Props): ReactElement => {
   return (
     <MediaPlayer src={src} preferNativeHLS={true} crossorigin="" ref={player}>
       <MediaOutlet />
+      <MediaPoster />
       <MediaCommunitySkin />
     </MediaPlayer>
   );
