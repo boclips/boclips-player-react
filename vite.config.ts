2c0387920f9ffde16ce1fde694cddc99bb53b487
Ed Cook <edward@boclips.com>
2024-12-09 11:09:06 +0000

    Inject css via js
    This (hopefully) allows us to import just the component and get the styling
    
    [sc-1855]
    
    Signed-off-by: Harsha Chaudhary <harsha@boclips.com>

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 import { resolve } from 'path';
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig(({ command }) => ({
   ...(command === 'serve' && {
@@ -28,5 +29,6 @@ export default defineConfig(({ command }) => ({
         content,
       }),
     }),
+    cssInjectedByJsPlugin(),
   ],
 }));
